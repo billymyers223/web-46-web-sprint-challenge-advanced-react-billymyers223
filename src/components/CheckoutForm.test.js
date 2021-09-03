ffa7ec55e46361837ce042a8ab7ed7996a3ff5f9
billmyers223 <billymyers223@gmail.com>
2021-09-03 13:04:54 -0500

    Added tests going to add more to validate all form details

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, waitFor, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -9,8 +10,28 @@ test("renders without errors", () => {
     render (<CheckoutForm/>)
 });
 
-test("shows success message on submit with form details", () => {
+test("shows success message on submit with form details", async () => {
     render (<CheckoutForm/>)
+    
+    const firstInput = screen.getByLabelText(/First name:/i);
+    userEvent.type(firstInput, 'Billy')
+    const lastInput = screen.getByLabelText(/Last name:/i);
+    userEvent.type(lastInput, 'Myers')
+    const addInput = screen.getByLabelText(/Address:/i);
+    userEvent.type(addInput, '1234 Drive Lane')
+    const cityInput = screen.getByLabelText(/City:/i);
+    userEvent.type(cityInput, 'Houston')
+    const stateInput = screen.getByLabelText(/State:/i);
+    userEvent.type(stateInput, 'Texas')
+    const zipInput = screen.getByLabelText(/Zip:/i);
+    userEvent.type(zipInput,'77449');
 
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    await waitFor(() =>{
+        const success = screen.getByTestId('successMessage')
+        expect(success).toBeInTheDocument();
+    })
 
 });
